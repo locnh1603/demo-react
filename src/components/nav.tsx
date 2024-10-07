7d16e37f1233c69f991f288ca765bb8974c8aac3
Loc Nguyen Huu <nghloc1603@gmail.com>
2024-10-07 09:23:16 +0700

    Remove unused html tags, update app title and favico

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,13 +1,9 @@
-import {AppBar, IconButton, Toolbar, Typography} from '@mui/material';
-import {Menu} from '@mui/icons-material';
+import {AppBar, Toolbar, Typography} from '@mui/material';
 
 const Nav = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-          <Menu />
-        </IconButton>
         <Typography variant="h6" color="inherit" component="div">
           Task Management
         </Typography>
