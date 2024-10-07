a59a3cc67075f3b7d5f2386566f8bd8a140b9de6
Loc Nguyen Huu <nghloc1603@gmail.com>
2024-10-07 10:50:44 +0700

    Implement fetch request for init data

--- src/routes/tasks.tsx
+++ src/routes/tasks.tsx
@@ -1,9 +1,26 @@
-import {useContext} from 'react';
+import {useContext, useEffect} from 'react';
 import {Context} from './layout.tsx';
 import Table from '../components/table.tsx';
+import {TaskModel} from '../models/task.model.ts';
+import moment from 'moment';
 
 const Tasks = () => {
-  const {tasks} = useContext(Context);
+  const {tasks, initTasks} = useContext(Context);
+  const url = 'https://dummyjson.com/c/5647-88d9-4675-8c22';
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data: TaskModel[]) => {
+        console.log(data);
+        data.forEach(item => {
+          item.createdDate = moment(item.createdDate).toDate();
+          item.dueDate = moment(item.dueDate).toDate();
+        });
+        initTasks(data);
+      });
+  }, []);
   const headers = [
     {title: 'Task Name', field: 'taskName'},
     {title: 'Created Date', field: 'createdDate', render: 'date'},
